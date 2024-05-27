f8bbad67124cc7096338e989347bb84e3750e670
Aliasgar Khimani <khimanialiasgar@gmail.com>
2024-05-27 13:57:21 +0300

    BREAK: kivymd breaks on navigationbar

--- components/settings_list.py
+++ components/settings_list.py
@@ -5,24 +5,15 @@ from kivy.config import Config
 Config.set('graphics', 'width', '450')
 Config.set('graphics', 'height', '900')
 
-from kivymd.app import MDApp
-from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
 from kivymd.uix.screen import MDScreen
-from kivy.uix.screenmanager import SlideTransition
-from kivy_garden.mapview import MapView, MapMarker
 from kivy.uix.floatlayout import FloatLayout
-from kivymd.uix.button import MDFabButton
-from kivymd.uix.appbar import MDTopAppBar
 from kivy.uix.image import Image
-
-class SettingsView(MDScreen):
-    items = [
-        {"icon": "account", "text": "Account"},
-        {"icon": "bell", "text": "Notifications"},
-        {"icon": "cog", "text": "Tab 3"},
-        {"icon": "help", "text": "Help"},
-    ]
-        
-class SettingsListItem(MDNavigationItem):
-    icon = StringProperty()
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivymd.uix.list import OneLineAvatarListItem
+      
+class SettingsListItem(OneLineAvatarListItem):
     text = StringProperty()
+    source = StringProperty()
+
+class SettingsScreen(Screen):
+    pass
